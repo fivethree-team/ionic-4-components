9207699429a16de3aefaa72b1dbe1c975be56fd6
Gary Großgarten <gary.grossgarten@gmail.com>
2018-09-09 00:19:59 +0200

    feat(item-exandable): change for custom header

--- projects/layout/src/lib/item-expandable/item-expandable.component.ts
+++ projects/layout/src/lib/item-expandable/item-expandable.component.ts
@@ -30,71 +30,31 @@ import { Color } from "@ionic/core";
     ]
 })
 export class ItemExpandableComponent implements OnInit {
-
-    /**
-     * Color used for the item.
-     * Default options are: `"primary"`, `"secondary"`, `"tertiary"`, `"success"`, `"warning"`, `"danger"`, `"light"`, `"medium"`, and `"dark"`.
-     */
-    @Input() color?: Color;
-
-    /**
-     * If true, the user cannot interact with the item. Defaults to `false`.
-     */
-    @Input() disabled = false;
-
-    /**
-     * If true, the content is expanded and visible to the user. Defaults to `false`.
-     */
-    @Input() expanded = false;
-
-    /**
-     * Icon displayed in the item.
-     */
-    @Input() icon?: string;
-
-    /**
-     * Icon rotating 180° when toggling the content. Defaults to `arrow-down`.
-     */
-    @Input() iconAnimated = 'ios-arrow-down';
-
-    /**
-     * Slot to position the icon in the item. Defaults to `end`.
-     */
-    @Input() iconAnimatedSlot: 'start' | 'end' = 'end';
-
-    /**
-     * Slot to position the icon in the item. Defaults to `start`.
-     */
-    @Input() iconSlot: 'start' | 'end' = 'start';
-
-    /**
-     * Text displayed in the item.
-     */
-    @Input() text?: string;
-
-    /**
-     * How the bottom border should be displayed on the item. 
-     * Available options: "full", "inset", "none". 
-     */
-    @Input() lines?: 'full' | 'inset' | 'none';
-
-    /**
-     * Emitted when the `expanded` state changes.
-     */
-    @Output() fivExpand: EventEmitter<boolean> = new EventEmitter<boolean>();
-
-    state: 'open' | 'closed';
-
-    constructor() { }
-
     ngOnInit(): void {
-        this.state = this.expanded ? 'open' : 'closed'
     }
 
-    toggleExpand() {
-        this.expanded = !this.expanded;
-        this.state = this.expanded ? 'open' : 'closed';
-        this.fivExpand.emit(this.expanded);
+    public listOpen: boolean = false;
+  state: string = 'closed';
+
+    constructor(){
+
+    }
+
+    openList() {
+        if (this.listOpen == false) {
+            this.listOpen = true;
+            this.state = 'open';
+        } else {
+            this.listOpen = false;
+            this.state = 'closed';
+  
+        }
+    }
+  
+    close() {
+        console.log('close');
+        this.listOpen = false;
+        this.state = 'closed';
     }
 
 }
