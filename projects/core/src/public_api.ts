55698af2ca6573533eb8268415281ab3acd47541
Marc Stammerjohann <marcstammerjohann@web.de>
2018-10-11 10:58:42 +0200

    chore(public_api): export components and service

--- projects/core/src/public_api.ts
+++ projects/core/src/public_api.ts
@@ -3,3 +3,20 @@
  */
 
 export * from './lib/fivethree.core.module';
+
+// export components
+export * from './lib/app-bar/app-bar.component';
+export * from './lib/expandable/expandable.component';
+export * from './lib/loading-button/loading-button.component';
+export * from './lib/loading-content/loading-content.component';
+export * from './lib/loading-fab/loading-fab.component';
+export * from './lib/loading-progress-bar/loading-progress-bar.component';
+export * from './lib/password/password.component';
+export * from './lib/router-item/router-item.component';
+export * from './lib/step/step.component';
+export * from './lib/step-content/step-content.component';
+export * from './lib/step-header/step-header.component';
+export * from './lib/stepper/stepper.component';
+
+// export services
+export * from './lib/loading/loading.service';
