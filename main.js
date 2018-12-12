9a2f442d54f24cfa553636f8eacb537e6eecaa14
Gary Großgarten <gary.grossgarten@gmail.com>
2018-12-12 03:47:13 +0100

    Auto-generated commit

--- main.js
+++ main.js
@@ -7270,71 +7270,71 @@ var AppComponent = /** @class */ (function () {
         this.splashScreen = splashScreen;
         this.statusBar = statusBar;
         this.appPages = [
-            {
-                title: 'App Bar',
-                url: '/app-bar',
-                icon: 'git-commit'
-            },
-            {
-                title: 'Bottom Sheet',
-                url: '/bottom-sheet',
-                icon: 'ios-arrow-up'
-            },
-            {
-                title: 'Dialog',
-                url: '/dialog',
-                icon: 'notifications'
-            },
-            {
-                title: 'Expandable',
-                url: '/expandable',
-                icon: 'resize'
-            },
-            {
-                title: 'Icon',
-                url: '/icon',
-                icon: 'mail-unread'
-            },
-            {
-                title: 'Image View',
-                url: '/image',
-                icon: 'image'
-            },
+            // {
+            //   title: 'App Bar',
+            //   url: '/app-bar',
+            //   icon: 'git-commit'
+            // },
+            // {
+            //   title: 'Bottom Sheet',
+            //   url: '/bottom-sheet',
+            //   icon: 'ios-arrow-up'
+            // },
+            // {
+            //   title: 'Dialog',
+            //   url: '/dialog',
+            //   icon: 'notifications'
+            // },
+            // {
+            //   title: 'Expandable',
+            //   url: '/expandable',
+            //   icon: 'resize'
+            // },
+            // {
+            //   title: 'Icon',
+            //   url: '/icon',
+            //   icon: 'mail-unread'
+            // },
+            // {
+            //   title: 'Image View',
+            //   url: '/image',
+            //   icon: 'image'
+            // },
             {
                 title: 'Loading Indicators',
                 url: '/loading',
                 icon: 'time'
             },
-            {
-                title: 'Map',
-                url: '/map',
-                icon: 'map'
-            },
-            {
-                title: 'Password Item',
-                url: '/password',
-                icon: 'key'
-            },
-            {
-                title: 'Refresher',
-                url: '/refresh',
-                icon: 'refresh'
-            },
-            {
-                title: 'Searchbar',
-                url: '/toolbar-search',
-                icon: 'search'
-            },
+            // {
+            //   title: 'Map',
+            //   url: '/map',
+            //   icon: 'map'
+            // },
+            // {
+            //   title: 'Password Item',
+            //   url: '/password',
+            //   icon: 'key'
+            // },
+            // {
+            //   title: 'Refresher',
+            //   url: '/refresh',
+            //   icon: 'refresh'
+            // },
+            // {
+            //   title: 'Searchbar',
+            //   url: '/toolbar-search',
+            //   icon: 'search'
+            // },
             {
                 title: 'Stepper',
                 url: '/stepper',
                 icon: 'share'
             },
-            {
-                title: 'Directives',
-                url: '/directives',
-                icon: 'rocket'
-            },
+            // {
+            //   title: 'Directives',
+            //   url: '/directives',
+            //   icon: 'rocket'
+            // },
             {
                 title: 'Buttons',
                 url: '/buttons',
