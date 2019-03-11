1e286adb0b73b0a77f598dd911c13ad94746e28a
Gary Großgarten <gary.grossgarten@gmail.com>
2019-03-11 14:53:38 +0100

    feat(network-status): added fade in fade out animation

--- projects/core/src/lib/network-status/network-status.component.ts
+++ projects/core/src/lib/network-status/network-status.component.ts
@@ -1,9 +1,22 @@
 import { Component, OnInit, EventEmitter } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'fiv-network-status',
   templateUrl: './network-status.component.html',
   styleUrls: ['./network-status.component.scss'],
+  animations: [
+    trigger('status', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('200ms ease-out', style({ opacity: 1 }))
+      ]),
+      transition('* => void', [
+        style({ opacity: 1 }),
+        animate('550ms 200ms ease-in', style({ opacity: 0 }))
+      ]),
+    ])
+  ]
 })
 export class NetworkStatusComponent implements OnInit {
   visible = true;
