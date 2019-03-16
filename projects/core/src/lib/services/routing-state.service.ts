adaf3160a72e8ae4f9284a84f401f38332c51776
Gary Großgarten <gary.grossgarten@gmail.com>
2019-03-16 15:22:31 +0100

    fix(back-button): fix configurable root

--- projects/core/src/lib/services/routing-state.service.ts
+++ projects/core/src/lib/services/routing-state.service.ts
@@ -5,7 +5,7 @@ import { NavController, Platform } from '@ionic/angular';
 
 export interface RoutingStateConfig {
   clearOnRoot?: boolean;
-  root: '/';
+  root: string;
 }
 
 @Injectable({
@@ -33,7 +33,7 @@ export class FivRoutingStateService {
       .subscribe(({ urlAfterRedirects }: NavigationEnd) => {
         // add url to history
         this.history = [...this.history, urlAfterRedirects];
-        if (this.config && this.config.clearOnRoot && urlAfterRedirects === this.config.root) {
+        if (this.config && this.config.clearOnRoot && urlAfterRedirects === (this.config.root || '/')) {
           // clear on root
           this.clearHistory();
         }
