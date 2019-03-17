f78db2981450f79ee7352fba73f9dfe806eb6e8a
Gary Großgarten <gary.grossgarten@gmail.com>
2019-03-17 17:07:08 +0100

    debug routing

--- projects/core/src/lib/services/routing-state.service.ts
+++ projects/core/src/lib/services/routing-state.service.ts
@@ -33,6 +33,8 @@ export class FivRoutingStateService {
       .subscribe(({ urlAfterRedirects }: NavigationEnd) => {
         // add url to history
         this.history = [...this.history, urlAfterRedirects];
+        console.log('CLEAR HISTORY', this.config, urlAfterRedirects, this.config && this.config.clearOn &&
+          this.config.clearOn.some(s => s === urlAfterRedirects));
 
         if (this.config && this.config.clearOn &&
           this.config.clearOn.some(s => s === urlAfterRedirects)) {
