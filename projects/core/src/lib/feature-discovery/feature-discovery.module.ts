9f632327f3970ddd195b276c2087f7a59ea1aea4
Marc Stammerjohann <marcstammerjohann@web.de>
2019-04-19 22:39:14 +0200

    feat(feature-discovery): update module

--- projects/core/src/lib/feature-discovery/feature-discovery.module.ts
+++ projects/core/src/lib/feature-discovery/feature-discovery.module.ts
@@ -3,14 +3,22 @@ import { FivFeature } from './feature.directive';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FivFeatureDiscovery } from './feature-discovery.component';
+import { FivIconModule } from '../icon/icon.module';
 
 @NgModule({
-    declarations: [FivFeature],
+    declarations: [
+        FivFeature,
+        FivFeatureDiscovery
+    ],
     imports: [
         CommonModule,
+        FivIconModule,
         FivRippleModule
     ],
-    exports: [FivFeature],
+    exports: [
+        FivFeature,
+        FivFeatureDiscovery
+    ],
     entryComponents: [FivFeatureDiscovery]
 })
 export class FivFeatureDiscoveryModule { }
